08f404e9cda080dbe1ca9696cb4f008e0669bc29
Unknown <ned36@cornell.edu>
2020-04-30 14:57:14 -0400

    Simulate annotations
    Simulated annotations look good but because of short archetypes the loci are grouped more closely. Need to improve archetype selection? Maybe include data so inactive transcripts are ignored when looking for isolated transcripts. Also implemented clean data export and added jitter to re-sampling.

--- R/type_checkers.R
+++ R/type_checkers.R
@@ -8,3 +8,14 @@
 is.GRanges <- function(x) {
   return(class(x) == "GRanges")
 }
+
+#' is.GRangesList
+#'
+#' Checks if object is a \code{\link[GenomicRanges]{GRangesList-class}} object
+#'
+#' @return logical
+#'
+#' @export
+is.GRangesList <- function(x) {
+  return(class(x) == "GRangesList")
+}
